15a730f6ba3a63a9f1e71cd7457588a9633ac7cb
odlt98 <47251220+odlt98@users.noreply.github.com>
2019-02-07 11:18:22 -0600

    Add files via upload

---
+++ time2.py
@@ -0,0 +1,12 @@
+#Omar De La Torre
+
+#2/7/2019
+
+time = input("What time is it now?")
+wait_time = input("What is the number of hours to wait?")
+time = int(time)
+wait_time = int(wait_time)
+
+time_when_alarm_go_off = time + wait_time
+print (time_when_alarm_go_off)
+
