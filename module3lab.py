8fc8f5809ccee197b40a68071e685548eb2351ea
odlt98 <47251220+odlt98@users.noreply.github.com>
2019-02-10 16:22:09 -0600

    Add files via upload

---
+++ module3lab.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+#Omar De La Torre
+#1/31/2019
+
+name = str(input("What is your name? "))
+print ("Hello" ,name)
---
+++ temp conversion.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+#Omar De La Torre
+#1/31/2019
+
+#Converts degrees Fahrenheit to degrees Celsius
+
+f = int(input("Enter degrees farenheit: ")) #(f − 32) × 1.8
+c = (f - 32) * 0.5555
+print("Temperature:", f, "degrees farenheit = ", c, "degrees celcius")
